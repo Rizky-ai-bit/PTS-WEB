f02773cfa70d6a55405ce1af3f828522d2620fa2
Kyy <rizkygamimg17@gmail.com>
2025-05-08 08:15:12 +0700

    Add files via upload

---
+++ rizkyal.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2025 at 03:14 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rizkyal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_profil`
+--
+
+CREATE TABLE `tb_profil` (
+  `nama` varchar(100) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` enum('laki-laki','perempuan') NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_profil`
+--
+
+INSERT INTO `tb_profil` (`nama`, `nim`, `tgl_lahir`, `jk`, `alamat`) VALUES
+('Rizky', 12345678, '2008-04-10', 'laki-laki', 'Gumulung');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
